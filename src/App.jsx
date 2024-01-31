dddbb46ec6246996d71cb9cd9fa1762ccf0ba4e5
Bhasakr Roy <i.bhskr@hotmail.com>
2024-02-01 01:58:43 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/navbar";
 import BodyA from "./components/BodyA";
 import Workdomain from "./components/Workdomain";
 import Project from "./components/Project";
 import Education from "./components/Education";
 import Footer from "./components/Footer";
+
 function App() {
+  
   return (
     <div>
       <Navbar />
