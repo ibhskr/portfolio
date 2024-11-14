ad35dcf28297a27f9bb913211678e745064157b1
Bhasakr Roy <i.bhskr@hotmail.com>
2024-11-14 23:03:40 +0530

    ui updated

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,7 +9,7 @@ function Education() {
         <StaggerText
           staggerType="letter"
           staggerDuration={1}
-            startDelay={0.06}
+          startDelay={0.06}
           // startDelay={500}
         >
           Education
@@ -17,35 +17,25 @@ function Education() {
         <img src={underlineImage} alt="" srcset="" />
       </div>
       <div className="educationsection ">
-        <div className="ed-item ed-item-1">
-          <img src="https://www.svgrepo.com/show/402648/school.svg" alt="" />
-          <div className="ed-text">
+        <div className="ed-item ">
+          <p className="ed-tag">High School</p>
+          <div>
             <h3>Baneswar Khabsa High School</h3>
             <p>12th with PCM</p>
             <p>Academic Year: 2016 - 2018</p>
           </div>
         </div>
-        <div className="ed-item ed-item-2">
-          <img
-            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/853250/college-building-clipart-md.png"
-            alt=""
-          />
-          <div className="ed-text">
-            <h3>Jalpaiguri Polytechnic Institute</h3>
-            <p>Diploma in Mechanical Engineering</p>
-            <p>Academic Year: 2018 - 2021</p>
-          </div>
+        <div className="ed-item ">
+          <p className="ed-tag">Diploma</p>
+          <h3>Jalpaiguri Polytechnic Institute</h3>
+          <p>Diploma in Mechanical Engineering</p>
+          <p>Academic Year: 2018 - 2021</p>
         </div>
-        <div className="ed-item ed-item-3">
-          <img
-            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3518094/college-building-clipart-md.png"
-            alt=""
-          />
-          <div className="ed-text">
-            <h3>CoochBehar Govt. Engineering College</h3>
-            <p>Computer Science and Engineering</p>
-            <p>Academic Year: 2022 - 2025</p>
-          </div>
+        <div className="ed-item ">
+          <p className="ed-tag">B.Tech</p>
+          <h3>CoochBehar Govt. Engineering College</h3>
+          <p>Computer Science and Engineering</p>
+          <p>Academic Year: 2022 - 2025</p>
         </div>
       </div>
     </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import xClone from "../assets/xClone.png";
 import findMyHotel from "../assets/findMyHotel.png";
 import underlineImage from "../assets/yellow-vector.svg";
+import gitHubExp from "../assets/gitHubExp.png";
 import StaggerText from "react-stagger-text";
 import "./Project.css";
+import { FaGithub } from "react-icons/fa";
+import { FaInternetExplorer } from "react-icons/fa";
+import { FaNpm } from "react-icons/fa";
 function Project() {
   return (
     <div className="project-section">
@@ -33,11 +37,14 @@ function Project() {
           </p>
 
           <a
-            href="https://github.com/ibhskr/x_clone"
+            href="https://github.com/ibhskr/xclone"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button>Visit</button>
+            <button>
+              <FaGithub />
+              Visit
+            </button>
           </a>
         </div>
         <div className="project-item">
@@ -54,7 +61,59 @@ function Project() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button>Visit</button>
+            <button>
+              <FaGithub />
+              Visit
+            </button>
+          </a>
+        </div>
+        <div className="project-item">
+          <img src={gitHubExp} alt="" />
+          <p>
+            • Developed{" "}
+            <span className="highlight">GitHub Profile Explorer</span>, This
+            application enables users to search for GitHub profiles and view
+            detailed information, such as repositories, followers, and
+            contributions. It features an intuitive and responsive user
+            interface designed for a seamless experience. The app provides easy
+            access to publicly available GitHub data without requiring users to
+            log in, making it a convenient tool for developers, recruiters, or
+            anyone exploring GitHub profiles.
+          </p>
+          <a
+            href="https://github-search-beryl-three.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>
+              <FaInternetExplorer />
+              Visit
+            </button>
+          </a>
+        </div>
+        <div className="project-item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/1920px-Npm-logo.svg.png"
+            alt=""
+          />
+          <p>
+            • Developed{" "}
+            <span className="highlight">Time And Date Calculate Package</span>,
+            This is a simple JavaScript based package that takes a date string
+            and returns how much time has passed since that date in a
+            human-readable format, such as "2 hours ago", "3 days ago", etc. You
+            can use it in any web development project to display time
+            differences dynamically.
+          </p>
+          <a
+            href="https://www.npmjs.com/package/calculate-time-date"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>
+              <FaNpm size="30px" />
+              Visit
+            </button>
           </a>
         </div>
       </div>
